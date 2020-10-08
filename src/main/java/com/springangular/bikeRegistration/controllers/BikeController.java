813e0db92a2b6a03cd988af6c6f4cc03fda4fd0d
Adelina <apetreiadelina@yahoo.com>
2020-10-09 01:54:31 +0300

    Added automatic mail sending after submitting the bike form.

--- src/main/java/com/springangular/bikeRegistration/controllers/BikeController.java
+++ src/main/java/com/springangular/bikeRegistration/controllers/BikeController.java
@@ -1,18 +1,25 @@
 package com.springangular.bikeRegistration.controllers;
 
 import com.springangular.bikeRegistration.entities.Bike;
+import com.springangular.bikeRegistration.model.MailModel;
 import com.springangular.bikeRegistration.services.BikeService;
+import com.springangular.bikeRegistration.services.SendingEmailService;
+import freemarker.template.TemplateException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 public class BikeController {
     private final BikeService bikeService;
+    private final SendingEmailService sendingEmailService;
 
-    public BikeController(BikeService bikeService) {
+    public BikeController(BikeService bikeService, SendingEmailService sendingEmailService) {
         this.bikeService = bikeService;
+        this.sendingEmailService = sendingEmailService;
     }
 
     @GetMapping("api/v1/bikes")
@@ -28,6 +35,16 @@ public class BikeController {
     @PostMapping("/api/v1/bikes")
     public void saveBike(@RequestBody Bike bike){
         bikeService.createBike(bike);
+    }
+
+    @PostMapping("/api/v2/bikes")
+    public void saveBikeAndSendEmail(@RequestBody Bike bike, MailModel mailModel){
+        bikeService.createBike(bike);
+        try {
+            sendingEmailService.sendEmail(mailModel, bike.getName(), bike.getEmail());
+        } catch (MessagingException | IOException | TemplateException e) {
+            e.printStackTrace();
+        }
     }
 
     @GetMapping("api/v1/bikes/sellers/{seller}")
